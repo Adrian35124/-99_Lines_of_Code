2da7749003e641f6e557048ab24053ecac24fa8f
adrian35124 <adrian35124@gmail.com>
2020-05-11 15:33:34 -0500

    Almost FInised

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let friends = ["Jon" + "Brian" + "Jose" + "Rodrigo" + "Jessica"]
 for (let i = 0; i < friends.length; i++) {
     let num = 99;
+    while (num >0) {
+        console.log(num + "files of code in the file");
+        console.log(num + "files lines of code.");
+        console.log(friends[i] + "strikes one out, clears it all out")
+        num = num - 1;
+        if (num > 0)
+        {
+            console.log(num + "files of code in the files.");
+        }
+        else{
+            console.log("No More files of code in the file");
+        }
+    }
+
 }
- 
+
 
