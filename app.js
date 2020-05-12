ee7973c0e4b1e3a2e80ec90f0efbda3b28652ff5
adrian35124 <adrian35124@gmail.com>
2020-05-12 13:22:02 -0500

    Ran into a problem

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let friends = ["Jon", "Brian", "Jose", "Rodrigo", "Jessica"]
 for (let i = 0; i < friends.length; i++) {
    for (let index = 99; index > 0; index--) {
-       if(index > 2) {
-           console.log(index + " lines of code in the file, " + index + " lines of code, " + friends[i] + " takes one out debugs it out, " + (index - 1) + ' lines of code in the file!') 
-       } 
-       
-
-   }
+       if (index === 2)
+       console.log(index + " line of code in the file, " + index + " line of code; " + friends[i] + " strikes one out, clears it all out, " + (index - 1) + 'line of code in the file!');
+   } else if(index === 1) {
+    console.log(index + " lines of code in the file, " + index + " lines of code; " + friends[i] + " strikes one out, clear it all out, no more lines of code in the files");
+   } else {
+           console.log(index + " lines of code in the file, " + index + " lines of code; " + friends[i] + " takes one out debugs it out, " + (index - 1) +  "lines of code in the file!") 
+        }
+   } 
 }
